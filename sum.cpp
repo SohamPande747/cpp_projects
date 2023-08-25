bf3a2b944b4133fa46141ba1b475432bbb0b3146
Soham Pande <sohampande747@gmail.com>
2023-08-26 01:43:30 +0530

    can be ignored

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int num1, num2;
+
+
+    std :: cout <<"Enter value of num1: " ;
+    std :: cin >>num1;
+    std :: cout <<"Enter value of num2: " ;
+    std :: cin >>num2;
+
+    std :: cout<<"The Sum is: "<<num1+num2;
+
+
+    return 0;
+    
+}
